723bd607739550aa42a9737a3ae9b74ccea9e5fd
eric <butlerericm@gmail.com>
2022-10-20 15:51:47 -0400

    Added optical flow test

--- video_analyzer_2.py
+++ video_analyzer_2.py
@@ -242,7 +242,23 @@ class DisplayApp:
         self.writeout(tracks)
 
     def test(self):
-        pass
+        hsv = np.zeros_like(self.vid.get_frame(0)[1])
+        hsv[..., 1] = 255
+        start = 1
+        for x in range(start, int(self.vid.count_frames())):
+            ret_old, f_old = self.vid.get_frame(x - 1)
+            ret_new, f_new = self.vid.get_frame(x)
+            f_old = cv2.cvtColor(f_old, cv2.COLOR_BGR2GRAY)
+            f_new = cv2.cvtColor(f_new, cv2.COLOR_BGR2GRAY)
+            flow = cv2.calcOpticalFlowFarneback(f_old, f_new, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+            hsv[..., 0] = ang * 180 / np.pi / 2
+            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+            bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(bgr))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=NW)
+            self.canvas.update_idletasks()
+            self.Notifications.insert((1.0), 'Frame ' + str(x) + '\n')
 
     def writeout(self, tracks):
         with open('Tracks.csv', 'w', newline='') as csvfile:
