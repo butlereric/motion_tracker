102e36b7fbc8081733d189d5262204d731cb0b2b
Eric Butler <butlerericm@gmail.com>
2022-10-20 23:03:35 -0400

    Optical flow works and exists.

--- video_analyzer_2.py
+++ video_analyzer_2.py
@@ -3,8 +3,6 @@ from tkinter import *
 from tkinter import filedialog
 import PIL.Image, PIL.ImageTk
 import numpy as np
-import colorsys
-import matplotlib
 import csv
 
 tracks_enabled = True
@@ -57,6 +55,10 @@ class DisplayApp:
         self.start_after_hist.set(1)
         self.stopped = IntVar()
         self.stopped.set(0)
+        self.hist_text = StringVar()
+        self.hist_text.set('History Length')
+        self.thresh_text = StringVar()
+        self.thresh_text.set('Threshold')
         self.MainFrame = Frame(self.AppParent)
         self.MainFrame.pack()
         self.make_frames()
@@ -111,44 +113,67 @@ class DisplayApp:
         Label(self.ButtonFrame, text='').grid(row=row, column=column)
         row += 1
 
-        Radiobutton(self.ButtonFrame, text="Use MOG background subtraction", variable=self.background_subtractor, value='MOG').grid(row=row,
+        Radiobutton(self.ButtonFrame, text="Use MOG background subtraction", variable=self.background_subtractor,
+                    value='MOG', command=self.change_labels_to_background_subtract).grid(row=row,
                                                                                                               column=column)
         row += 1
         Radiobutton(self.ButtonFrame, text="Use KNN background subtraction", variable=self.background_subtractor,
-                    value='KNN').grid(row=row,
+                    value='KNN', command=self.change_labels_to_background_subtract).grid(row=row,
                                     column=column)
+        row += 1
+        Radiobutton(self.ButtonFrame, text="Use optical flow", variable=self.background_subtractor,
+                    value='Flow', command=self.change_labels_to_optical_flow).grid(row=row,
+                                      column=column)
         row += 1
         Label(self.ButtonFrame, text='').grid(row=row,column=column)
         row += 1
 
         Label(self.ButtonFrame, text="Variables relating to analysis").grid(row=row,column=column)
         row += 1
-        Label(self.ButtonFrame, text="History length").grid(row=row, column=column)
+        self.hist_label = Label(self.ButtonFrame, textvariable=self.hist_text)
+        self.hist_label.grid(row=row, column=column)
         row += 1
         self.histEntry = Entry(self.ButtonFrame)
         self.histEntry.grid(row=row, column=column)
         self.histEntry.insert(0, '50')
         row += 1
-        Label(self.ButtonFrame, text="Threshold").grid(row=row, column=column)
+        self.thresh_label = Label(self.ButtonFrame, textvariable=self.thresh_text)
+        self.thresh_label.grid(row=row, column=column)
         row += 1
         self.varEntry = Entry(self.ButtonFrame)
         self.varEntry.grid(row=row, column=column)
         self.varEntry.insert(0, '20')
         row += 1
-        Label(self.ButtonFrame, text="Size filter").grid(row=row, column=column)
+        Label(self.ButtonFrame, text="Minimum size filter").grid(row=row, column=column)
         row += 1
-        self.size_filter = Entry(self.ButtonFrame)
-        self.size_filter.grid(row=row, column=column)
-        self.size_filter.insert(0, '70')
+        self.size_filter_min = Entry(self.ButtonFrame)
+        self.size_filter_min.grid(row=row, column=column)
+        self.size_filter_min.insert(0, '20')
         row += 1
-        Button(self.ButtonFrame, text="Analyze", command=self.movers_background_subtract).grid(row=row, column=column)
+
+        Label(self.ButtonFrame, text="Maximum size filter").grid(row=row, column=column)
         row += 1
-        Button(self.ButtonFrame, text="Test", command=self.test).grid(row=row, column=column)
+        self.size_filter_max = Entry(self.ButtonFrame)
+        self.size_filter_max.grid(row=row, column=column)
+        self.size_filter_max.insert(0, '200')
         row += 1
+
+        Button(self.ButtonFrame, text="Analyze", command=self.analyze).grid(row=row, column=column)
+        row += 1
+        #Button(self.ButtonFrame, text="Test", command=self.test).grid(row=row, column=column)
+        #row += 1
         self.stopped_button = Checkbutton(self.ButtonFrame, text='Stop running analysis', variable=self.stopped,
                                           onvalue=1, offvalue=0, indicatoron=False)
         self.stopped_button.grid(row=row, column=column)
         row += 1
+
+    def change_labels_to_background_subtract(self):
+        self.thresh_text.set('Threshold')
+        self.hist_text.set('History Length')
+
+    def change_labels_to_optical_flow(self):
+        self.thresh_text.set('Intensity threshold')
+        self.hist_text.set('Averaging window size')
 
     def openvideofile(self):
         path = filedialog.askopenfilename()
@@ -181,12 +206,33 @@ class DisplayApp:
         self.framenum = int(x)
         self.showframe()
 
+    def analyze(self):
+        if self.background_subtractor.get() == 'Flow':  # use optical flow
+            self.optical_flow()
+        else:
+            self.movers_background_subtract()
+
+    def size_filter_func(self, contours):
+        min_size = int(self.size_filter_min.get())
+        max_size = int(self.size_filter_max.get())
+        bigconts = []
+        templist = []  # holds mover points for this iteration
+        for cont in contours:
+            area = cv2.contourArea(cont)
+            if max_size >= area >= min_size:
+                bigconts.append(cont)
+                M = cv2.moments(cont)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                (x, y), radius = cv2.minEnclosingCircle(cont)
+                templist.append([cX, cY, area, x, y, radius])
+        return bigconts, templist
+
     def movers_background_subtract(self):
         self.stopped_button.deselect()  # un-stop us when you click this
         # grab weights
         hist = int(self.histEntry.get())
         var = int(self.varEntry.get())
-        size_filter = int(self.size_filter.get())
         if self.background_subtractor.get() == 'MOG':
             fgbg = cv2.createBackgroundSubtractorMOG2(history=hist, varThreshold=var, detectShadows=self.shadows)
         else:
@@ -202,15 +248,7 @@ class DisplayApp:
             fgmask = fgbg.apply(f)
             # analyze
             contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            bigconts = []
-            templist = []  # holds mover points for this iteration
-            for cont in contours:
-                if cv2.contourArea(cont) > size_filter:
-                    bigconts.append(cont)
-                    M = cv2.moments(cont)
-                    cX = int(M["m10"] / M["m00"])
-                    cY = int(M["m01"] / M["m00"])
-                    templist.append([cX, cY])
+            bigconts, templist = self.size_filter_func(contours)
             moverslist.append(templist)
             # make a blank mask
             tempmask = np.zeros(self.mask.shape, dtype=np.uint8)
@@ -229,7 +267,7 @@ class DisplayApp:
                 self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(tempmask.astype(np.uint8)))
             else:  # must be normal frame with size filtered motion
                 for m in moverslist[-1]:
-                    cv2.circle(f, (m[0], m[1]), size_filter + 5, (255, 0, 0))
+                    cv2.circle(f, (int(m[3]), int(m[4])), int(m[5]), (255, 0, 0))
                 self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(f))
             self.canvas.create_image(0, 0, image=self.photo, anchor=NW)
             self.canvas.update_idletasks()
@@ -253,28 +291,63 @@ class DisplayApp:
                 print('Could not save screenshot')
         self.writeout(tracks)
 
-    def test(self):
+    def optical_flow(self):
         self.stopped_button.deselect()
+        intensity_thresh = int(self.varEntry.get())
+        winsize = int(self.histEntry.get())
         hsv = np.zeros_like(self.vid.get_frame(0)[1])
         hsv[..., 1] = 255
+        # set up list to take positions of movers
+        moverslist = []
         start = 1
         for x in range(start, int(self.vid.count_frames())):
             ret_old, f_old = self.vid.get_frame(x - 1)
             ret_new, f_new = self.vid.get_frame(x)
-            f_old = cv2.cvtColor(f_old, cv2.COLOR_BGR2GRAY)
-            f_new = cv2.cvtColor(f_new, cv2.COLOR_BGR2GRAY)
-            flow = cv2.calcOpticalFlowFarneback(f_old, f_new, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            f_old_gr = cv2.cvtColor(f_old, cv2.COLOR_BGR2GRAY)
+            f_new_gr = cv2.cvtColor(f_new, cv2.COLOR_BGR2GRAY)
+            flow = cv2.calcOpticalFlowFarneback(f_old_gr, f_new_gr, None, 0.5, 3, winsize, 3, 5, 1.2, 0)
             mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-            hsv[..., 0] = ang * 180 / np.pi / 2
-            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+            #hsv[..., 2] = ang * 180 / np.pi / 2
+            hsv[..., 2] = 100
+            hsv[..., 0] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+            hsv[..., 0][hsv[..., 0] < intensity_thresh] = 0
             bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(bgr))
+            t, thresholded = cv2.threshold(hsv[..., 0], intensity_thresh, 255, cv2.THRESH_BINARY)
+
+            # analyze
+            contours, hierarchy = cv2.findContours(thresholded, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            bigconts, templist = self.size_filter_func(contours)
+            moverslist.append(templist)
+
+            # make a blank mask
+            tempmask = np.zeros(self.mask.shape, dtype=np.uint8)
+            # write big contours onto tempmask
+            for c in bigconts:
+                tempmask = cv2.drawContours(tempmask, [c], 0, (255), -1)
+            tempmask = np.dstack((tempmask, tempmask, tempmask))
+            for m in moverslist[-1]:  # most recent movers
+                cv2.circle(tempmask, (m[0], m[1]), 5, (255, 0, 0), -1)
+
+            # display
+            if self.show_mode.get() == 'R':
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(f_new))
+            elif self.show_mode.get() == 'M':
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(bgr))
+            elif self.show_mode.get() == 'F':
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(tempmask.astype(np.uint8)))
+            else:  # must be normal frame with size filtered motion
+                for m in moverslist[-1]:
+                    cv2.circle(f_new, (int(m[3]), int(m[4])), int(m[5]), (255, 0, 0))
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(f_new))
             self.canvas.create_image(0, 0, image=self.photo, anchor=NW)
             self.canvas.update_idletasks()
-            self.Notifications.insert((1.0), 'Frame ' + str(x) + ':' + str(self.stopped.get()) + '\n')
+            self.Notifications.insert((1.0), 'Frame ' + str(x) + '\n')
             self.AppParent.update()
             if self.stopped.get() == 1:
                 break
+
+    def test(self):
+        pass
 
     def writeout(self, tracks):
         with open('Tracks.csv', 'w', newline='') as csvfile:
@@ -286,22 +359,22 @@ class DisplayApp:
         tracks = []
         for m in moverslist: # go through frames
             for mover in m:
-                [cX,cY] = mover
+                [cX, cY, area, x1, y1, radius] = mover
                 d = 200
                 track = None
                 if len(tracks) <= 0:
-                    tracks.append([[cX, cY]])
+                    tracks.append([[cX, cY, area]])
                 else:
                     for x in range(0, len(tracks)-1):
                         t = tracks[x][-1]
-                        dist = ((cX - t[0])**2)+((cY-t[1])**2)
+                        dist = ((cX - t[0]) ** 2) + ((cY - t[1]) ** 2)
                         if dist < d:
                             track = x
                             d = dist
                     if track != None:
-                        tracks[track].append([cX, cY])
+                        tracks[track].append([cX, cY, area])
                     else:
-                        tracks.append([[cX, cY]])
+                        tracks.append([[cX, cY, area]])
         largetracks = []
         for t in tracks:
             if len(t) > 10:
